87b83516f736ff99b3f4248f6bd135f776fd7575
Akhil Rajan <rajanakhil07@gmail.com>
2023-03-08 14:08:35 +0530

    added routes and controllers

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import bodyParser from "body-parser";
 
 import connect from "./config/database.js";
 import { PORT } from "./config/serverConfig.js";
+import apiRoutes from "./routes/index.js";
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api", apiRoutes);
 
-const setUpAndStartServer = async () => {
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-
+const setUpAndStartServer = () => {
   app.listen(PORT, async () => {
     console.log(`Server running on port ${PORT}`);
     await connect();
